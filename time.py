1f5f489fed3dc1af56b0b85bfcb192a27c9bafa6
JesusPrieto <jesusdavidprieto@gmail.com>
2025-02-23 14:48:08 -0500

    Tarea Time + Aplicacion

--- time.py
+++ time.py
@@ -41,6 +41,66 @@ def hora():
 #tiempo_actual = time.localtime() 
 #print(tiempo_actual)
 
-tiempo_actual = time.localtime()
-tiempo = time.strftime("%B %d %y %H:%M:%S", tiempo_actual)
-print(tiempo)
+# - Cambiar de formato al tiempo actual con stfftime
+#tiempo_actual = time.localtime()
+#tiempo = time.strftime("%B %d %y %H:%M:%S", tiempo_actual)
+#print(tiempo)
+
+# - Cabiar de formato al tiempo Universal
+#tiempo_actual = time.gmtime()
+#tiempo = time.strftime("%B %d %y %H:%M:%S", tiempo_actual)
+#print(tiempo)
+
+
+#Str Time - Analiza una cadena de tiempo y devuelve el objeto de tiempo
+#cadena_tiempo = '23 February, 2025'
+#tiempo = time.strptime(cadena_tiempo, '%d %B, %Y')
+#print(tiempo)
+
+
+#Convierte una tupla de tiempo en una cadena legible
+#tiempo_tupla = (2025, 2, 23, 14, 24,0,0,0,0)
+#cadena_tiempo = time.asctime(tiempo_tupla)
+#print(cadena_tiempo)
+
+# Convierte el tiempo en segundos desde que inicio el tiempo en nuestro pc
+#tiempo_tupla = (2025, 2, 23, 14, 24,0,0,0,0)
+#cadena_tiempo = time.mktime(tiempo_tupla)
+#print(cadena_tiempo)
+
+
+#------ Ejercicio de Datos:
+
+def solicitudDocumentacon():
+    try:
+        Archivo = str(input('''Ingrese el nombre del archivo a solicitar: 
+                            1. Reporte de Facturas
+                            2. Reporte de Compras
+                            3. Reporte de Pagos: '''))
+        tiempo_actual = time.localtime()
+        tiempo = time.strftime("%B %d %y %H:%M:%S", tiempo_actual)
+        match Archivo:
+            case "1" | "Reporte de Facturas":
+                Archivo = "Reporte de Facturas"
+                print(f'''
+                      --- Su Archivo de {Archivo} 
+                      fue solicitado, {tiempo} 
+                      Tiempo de Envio: 24 hrs--- ''')
+            case "2" | "Reporte de Compras":
+                Archivo = "Reporte de Compras"
+                print(f'''
+                      --- Su Archivo de {Archivo} 
+                      fue solicitado, {tiempo} 
+                      Tiempo de Envio: 24 hrs ---''')
+            case "3" | "Reporte de Pagos":
+                Archivo = "Reporte de Pagos"
+                print(f'''
+                      --- Su Archivo de {Archivo} 
+                      fue solicitado, {tiempo} 
+                      Tiempo de Envio: 24 hrs--- ''')
+    except ValueError:
+        print("Ingreso un dato incorrecto")
+    except TypeError:
+        print("Ingreso un dato incorrecto")
+
+solicitudDocumentacon()
