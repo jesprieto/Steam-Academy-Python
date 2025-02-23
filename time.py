84bbbdd47c7fda8d4f91f0452e9db6ca7a000170
JesusPrieto <jesusdavidprieto@gmail.com>
2025-02-23 14:11:35 -0500

    Tarea Time

--- time.py
+++ time.py
@@ -23,4 +23,24 @@ def hora():
     formato = time.strftime("%m-%d-%Y %H:%M")
     print(formato)
 
-hora()
+
+
+#---------------------------------------------------------------#
+#Tarea de time
+
+# - origen del tiempo de nuestro pc
+# print(time.ctime(0)) 
+
+# - Tiempo que ha pasado en Segundos desde el origen
+# print(time.time()) 
+
+# - transformas el tiempo en segundos desde el origen en formato legible
+# print(time.ctime(time.time()))  
+
+# - Otra manera de imprimir tiempo actual
+#tiempo_actual = time.localtime() 
+#print(tiempo_actual)
+
+tiempo_actual = time.localtime()
+tiempo = time.strftime("%B %d %y %H:%M:%S", tiempo_actual)
+print(tiempo)
